0eb8b6631e23825b9569136969d44c91b1149c04
PGG WEB S.R.L. <officepggweb@gmail.com>
2024-11-25 00:28:00 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles.css"; // Importăm fișierul CSS
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles.css'; // Importăm fișierul CSS
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
